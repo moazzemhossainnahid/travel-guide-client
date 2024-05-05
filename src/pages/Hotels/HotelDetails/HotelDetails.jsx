e4caeed3abfb37a9ec7c2c1e85e3b7daf3bec630
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-05-05 09:12:57 +0600

    show hotel image

--- src/pages/Hotels/HotelDetails/HotelDetails.jsx
+++ src/pages/Hotels/HotelDetails/HotelDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import useHotelDetails from '../../../Hooks/useHotelDetails';
 import { FaLocationDot } from 'react-icons/fa6';
+import { FaCheck, FaCheckCircle } from 'react-icons/fa';
 
 const HotelDetails = () => {
     const { id } = useParams();
@@ -10,10 +11,19 @@ const HotelDetails = () => {
     console.log(hotel);
     return (
         <div className='w-full py-7 px-3'>
-            <h3 className="text-3xl md:text-5xl font-bold">{hotel?.hotel_name}</h3>
+            <h3 className="text-3xl md:text-5xl font-bold uppercase">{hotel?.hotel_name}</h3>
             <p className="pt-3 flex gap-2 items-center font-semibold"> <FaLocationDot className='text-2xl' /> {hotel?.location} </p>
-            <div className="py-7">
-                <img src={hotel?.image} alt="hotel_image" className="rounded-xl h-96" />
+            <div className="py-7 flex flex-col md:flex-row gap-5 justify-center items-center">
+                <div className="w-full h-96">
+                    <img src={hotel?.image} alt="hotel_image" className="rounded-xl h-full" />
+                </div>
+                <div className="w-full h-96 grid grid-cols-2 items-center justify-center gap-3">
+                    <img src={`https://api.sharetrip.net/api/v1/hotel/image?key=HyANbffVjkBh1mA2CJLuNFZlI6UkKrgAbXWPt8bqt5XudxSGJg/auh/IeYO9o63FWll2h/tvGridd5Ar9ZmNcLK2zvqYu7SQFFOLep/AnjzvHum1pSdfWYcCEAHilkZUD00Yfpcvp/xci6Z3AnHIvw==`} alt="hotel_image" className="rounded-xl h-full" />
+                    <img src={`https://api.sharetrip.net/api/v1/hotel/image?key=HyANbffVjkBh1mA2CJLuNFZlI6UkKrgAbXWPt8bqt5XudxSGJg/auh/IeYO9o63FWll2h/tvGridd5Ar9ZmNcCEFubi9rHouwRuZp4uQi20xPSqh//CKB6j6ZURu8D8fqFqKTFqxbCSn9ubhYcckOA==`} alt="hotel_image" className="rounded-xl h-full" />
+                    <img src={`https://api.sharetrip.net/api/v1/hotel/image?key=HyANbffVjkBh1mA2CJLuNFZlI6UkKrgAbXWPt8bqt5XudxSGJg/auh/IeYO9o63FWll2h/tvGridd5Ar9ZmNcIWWBKLst4Qwe19fm/I1EZ791v7mdSo0UEvfVhe26qDYgyNyyukWsiudV1dv059kdQ==`} alt="hotel_image" className="rounded-xl h-full" />
+                    <img src={`https://api.sharetrip.net/api/v1/hotel/image?key=HyANbffVjkBh1mA2CJLuNFZlI6UkKrgAbXWPt8bqt5XudxSGJg/auh/IeYO9o63FWll2h/tvGridd5Ar9ZmNcHZltjsvNi1VIP0xloJrDTOYrBlObCk+APQkvGN34vAfOlYpCEp1vdb39WExIyOnxw==`} alt="hotel_image" className="rounded-xl h-full" />
+
+                </div>
             </div>
             <div className="">
                 <h3 className="text-3xl md:text-5xl font-bold">{hotel?.hotel_name}</h3>
@@ -80,10 +90,37 @@ const HotelDetails = () => {
 
                         }
                     </div>
+                    <p className="font-bold text-2xl">({(hotel?.rating?.total_rating / hotel?.rating?.total_people).toFixed(2)}/5)</p>
                 </div>
 
                 <p className="pt-3 flex gap-2 text-xl items-center font-semibold"> <FaLocationDot className='text-2xl' /> {hotel?.location} </p>
 
+            </div>
+            <div className="py-5">
+                <h3 className="text-2xl font-bold">About this hotel</h3>
+                <div className="py-5 space-y-3">
+                    <h4 className="text-xl font-bold">General</h4>
+                    <p className="">{hotel?.description}</p>
+                </div>
+                <div className="py-5">
+                    <h3 className="text-2xl md:text-3xl font-bold">Important - Please Note:</h3>
+                    <div className="pt-5">
+                        <h3 className="font-bold ">Additional Facts:</h3>
+                        <ul className='list-disc'>
+                            <li className='flex gap-2 items-center'> <FaCheckCircle className='text-xs' />Check in from: {hotel?.additional_info?.check_in}</li>
+                            <li className='flex gap-2 items-center'> <FaCheckCircle className='text-xs' />Check Out until: {hotel?.additional_info?.check_out}</li>
+                        </ul>
+                    </div>
+                    <div className="pt-5">
+                        <h3 className="font-bold ">Payment accepted by the property:</h3>
+                        <ul className='list-disc'>
+                            {
+                                hotel?.payment_accept?.map(p => <li className='flex gap-2 items-center'> <FaCheckCircle className='text-xs' /> {p}</li>)
+                            }
+
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className='overflow-hidden p-5 my-10'>
                 <iframe title='map' className='w-full lg:w-[95vw] h-[300px] mx-auto rounded-md' id="gmap_canvas" src={`https://maps.google.com/maps?q=%20${hotel?.hotel_name}%20%20bangladesh&t=&z=13&ie=UTF8&iwloc=&output=embed`} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
