1c8729bd4c9672b41434dc0f992a8f183de2811f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-05-08 12:45:29 +0600

    design cards

--- src/components/Dashboard/AdminDashboard/Dashboard/DBCards/DBCards.jsx
+++ src/components/Dashboard/AdminDashboard/Dashboard/DBCards/DBCards.jsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import auth from "../../../../../../firebase.init";
-import { FaArrowAltCircleRight, FaUsers } from "react-icons/fa";
+import { FaArrowAltCircleRight, FaHotel, FaJediOrder, FaTripadvisor, FaUsers } from "react-icons/fa";
+import { FaAddressBook, FaBookAtlas, FaTentArrowTurnLeft } from "react-icons/fa6";
 
 const DBCards = () => {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
-  const [jobs, setJobs] = useState([]);
+  const [tours, setTours] = useState([]);
   const [socializations, setSocializations] = useState([]);
   const [blogs, setBlogs] = useState([]);
   const [jobApplications, setJobApplications] = useState([]);
@@ -26,14 +27,14 @@ const DBCards = () => {
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/api/v1/jobs`, {
+    fetch(`http://localhost:5000/api/v1/tours`, {
       method: "GET",
       headers: {
         authorization: `bearer ${localStorage.getItem("accessToken")}`,
       },
     })
       .then((res) => res.json())
-      .then((data) => setJobs(data?.data?.result));
+      .then((data) => setTours(data?.data?.result));
   }, []);
 
   useEffect(() => {
@@ -71,7 +72,7 @@ const DBCards = () => {
 
 
   const blogsFilter = blogs && blogs?.filter(blog => blog?.email === user?.email);
-  const jobsFilter = jobs && jobs?.filter(job => job?.email === user?.email);
+  const jobsFilter = tours && tours?.filter(job => job?.email === user?.email);
   const socializationsFilter = socializations && socializations?.filter(s => s?.email === user?.email);
   const jobApplicationsFilter = jobApplications && jobApplications?.filter(ja => ja?.email === user?.email);
 
@@ -102,24 +103,21 @@ const DBCards = () => {
             </h2>
           </div>
         </div>
-        {/* Total Jobs */}
+        {/* Total Countries */}
         <div className="">
           <div className="flex items-center justify-between bg-[#17A2BB] p-3 rounded-t-xl">
             <div className="">
               <h3 className="text-3xl md:text-4xl font-bold py-2 text-white">
-                {jobsFilter?.length}
+                {tours?.length}
               </h3>
-              <h3 className="text-md font-bold text-white">Total Job Posts</h3>
+              <h3 className="text-md font-bold text-white">Total Countries</h3>
             </div>
             <div className="">
-              {/* <FontAwesomeIcon
-                className="text-[#42424281] text-3xl md:text-4xl"
-                icon={faCartShopping}
-              /> */}
+              <FaTentArrowTurnLeft className="text-[#42424281] text-3xl md:text-4xl" />
             </div>
           </div>
           <div
-            onClick={() => navigate("/cpanel/mjobs")}
+            onClick={() => navigate("/cpanel/mcountries")}
             className="bg-[#0c93ab] cursor-pointer py-2 text-center rounded-b-xl"
           >
             <h2 className="text-md text-white flex items-center justify-center gap-2">
@@ -128,24 +126,21 @@ const DBCards = () => {
             </h2>
           </div>
         </div>
-        {/* Total Blogs */}
+        {/* Total Hotels */}
         <div className="">
           <div className="flex items-center justify-between bg-[#219422] p-3 rounded-t-xl">
             <div className="">
               <h3 className="text-3xl md:text-4xl font-bold py-2 text-white">
                 {blogsFilter?.length}
               </h3>
-              <h3 className="text-md font-bold text-white">Total Blog Posts</h3>
+              <h3 className="text-md font-bold text-white">Total Hotels</h3>
             </div>
             <div className="">
-              {/* <FontAwesomeIcon
-                className="text-[#42424281] text-3xl md:text-4xl"
-                icon={faBraille}
-              /> */}
+              <FaHotel className="text-[#42424281] text-3xl md:text-4xl" />
             </div>
           </div>
           <div
-            onClick={() => navigate("/cpanel/mblogs")}
+            onClick={() => navigate("/cpanel/mhotels")}
             className="bg-[#186e1a] cursor-pointer py-2 text-center rounded-b-xl"
           >
             <h2 className="text-md text-white flex items-center justify-center gap-2">
@@ -155,24 +150,21 @@ const DBCards = () => {
           </div>
         </div>
 
-        {/* Total Socializations */}
+        {/* Total Hotel Booking */}
         <div className="">
           <div className="flex items-center justify-between bg-[#572194b9] p-3 rounded-t-xl">
             <div className="">
               <h3 className="text-3xl md:text-4xl font-bold py-2 text-white">
                 {socializationsFilter?.length}
               </h3>
-              <h3 className="text-md font-bold text-white">Total Socialization Posts</h3>
+              <h3 className="text-md font-bold text-white">Total Booked Hotels</h3>
             </div>
             <div className="">
-              {/* <FontAwesomeIcon
-                className="text-[#42424281] text-3xl md:text-4xl"
-                icon={faBookAtlas}
-              /> */}
+              <FaAddressBook className="text-[#42424281] text-3xl md:text-4xl" />
             </div>
           </div>
           <div
-            onClick={() => navigate("/cpanel/msocializations")}
+            onClick={() => navigate("/cpanel/mbookhotels")}
             className="bg-[#572194ea] cursor-pointer py-2 text-center rounded-b-xl"
           >
             <h2 className="text-md text-white flex items-center justify-center gap-2">
@@ -182,25 +174,46 @@ const DBCards = () => {
           </div>
         </div>
 
-        {/* Total Job Applications */}
+        {/* Total Tour Plan */}
         <div className="">
           <div className="flex items-center justify-between bg-[#ad5530] p-3 rounded-t-xl">
             <div className="">
               <h3 className="text-3xl md:text-4xl font-bold py-2 text-white">
-                {jobApplicationsFilter?.length}{" "}
+                {tours?.length}{" "}
               </h3>
-              <h3 className="text-md font-bold text-white">Total Job Applications</h3>
+              <h3 className="text-md font-bold text-white">Total Tour Plan</h3>
             </div>
             <div className="">
-              {/* <FontAwesomeIcon
-                className="text-[#42424281] text-3xl md:text-4xl"
-                icon={faBookmark}
-              /> */}
+              <FaTentArrowTurnLeft className="text-[#42424281] text-3xl md:text-4xl" />
             </div>
           </div>
           <div
-            onClick={() => navigate("/cpanel/mjobapplications")}
+            onClick={() => navigate("/cpanel/mtours")}
             className="bg-[#8f4626] cursor-pointer py-2 text-center rounded-b-xl"
+          >
+            <h2 className="text-md text-white flex items-center justify-center gap-2">
+              More Info{" "}
+              <FaArrowAltCircleRight className="pl-2 text-2xl" />{" "}
+            </h2>
+          </div>
+        </div>
+
+        {/* Total Tour Booking */}
+        <div className="">
+          <div className="flex items-center justify-between bg-[#ad303c] p-3 rounded-t-xl">
+            <div className="">
+              <h3 className="text-3xl md:text-4xl font-bold py-2 text-white">
+                {jobApplicationsFilter?.length}{" "}
+              </h3>
+              <h3 className="text-md font-bold text-white">Total Booked Tour</h3>
+            </div>
+            <div className="">
+              <FaBookAtlas className="text-[#42424281] text-3xl md:text-4xl" />
+            </div>
+          </div>
+          <div
+            onClick={() => navigate("/cpanel/mbooktours")}
+            className="bg-[#8f2632] cursor-pointer py-2 text-center rounded-b-xl"
           >
             <h2 className="text-md text-white flex items-center justify-center gap-2">
               More Info{" "}
