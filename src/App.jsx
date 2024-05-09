2a2874768b2e3847586637914aa83ae6929b06ee
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-05-09 20:51:34 +0600

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { ToastContainer } from 'react-toastify'
-import Footer from './Pages/Shared/Footer/Footer'
-import Header from './Pages/Shared/Header/Header'
-import ContactUS from './Pages/ContactUS/ContactUS'
-import AboutUS from './Pages/AboutUS/AboutUS'
-import Signin from './Pages/Auth/Signin/Signin'
-import Signup from './Pages/Auth/Signup/Signup'
-import Tours from './Pages/Tours/Tours'
-import Profile from './Components/Dashboard/UserDashboard/Profile/Profile'
-import Hotels from './Pages/Hotels/Hotels'
-import HotelDetails from './Pages/Hotels/HotelDetails/HotelDetails'
-import TourDetails from './Pages/Tours/TourDeatils/TourDetails'
-import Checkout from './Pages/Checkout/Checkout'
-import RequireAuth from './Components/Others/RequireAuth/RequireAuth'
-import Success from './Components/Others/Success/Success'
-import RequireAdmin from './Components/Others/RequireAdmin/RequireAdmin'
-import CPanel from './Components/Dashboard/AdminDashboard/Admin/CPanel'
-import AdDashboard from './Components/Dashboard/AdminDashboard/Dashboard/AdDashboard'
-import ManageUsers from './Components/Dashboard/AdminDashboard/ManageUsers/ManageUsers'
-import ManageOrders from './Components/Dashboard/AdminDashboard/ManageOrders/ManageOrders'
-import ManageBookedTours from './Components/Dashboard/AdminDashboard/ManageBookedTours/ManageBookedTours'
-import ManageTours from './Components/Dashboard/AdminDashboard/ManageTours/ManageTours'
-import ManageHotels from './Components/Dashboard/AdminDashboard/ManageCountries/ManageHotels'
-import NotFound from './Pages/NotFound/NotFound'
-import Home from './Pages/Home/Home'
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
+import ContactUS from './Pages/ContactUS/ContactUS';
+import AboutUS from './Pages/AboutUS/AboutUS';
+import Signin from './Pages/Auth/Signin/Signin';
+import Signup from './Pages/Auth/Signup/Signup';
+import Tours from './Pages/Tours/Tours';
+import Profile from './Components/Dashboard/UserDashboard/Profile/Profile';
+import Hotels from './Pages/Hotels/Hotels';
+import HotelDetails from './Pages/Hotels/HotelDetails/HotelDetails';
+import TourDetails from './Pages/Tours/TourDeatils/TourDetails';
+import Checkout from './Pages/Checkout/Checkout';
+import RequireAuth from './Components/Others/RequireAuth/RequireAuth';
+import Success from './Components/Others/Success/Success';
+import RequireAdmin from './Components/Others/RequireAdmin/RequireAdmin';
+import CPanel from './Components/Dashboard/AdminDashboard/Admin/CPanel';
+import AdDashboard from './Components/Dashboard/AdminDashboard/Dashboard/AdDashboard';
+import ManageUsers from './Components/Dashboard/AdminDashboard/ManageUsers/ManageUsers';
+import ManageOrders from './Components/Dashboard/AdminDashboard/ManageOrders/ManageOrders';
+import ManageBookedTours from './Components/Dashboard/AdminDashboard/ManageBookedTours/ManageBookedTours';
+import ManageTours from './Components/Dashboard/AdminDashboard/ManageTours/ManageTours';
+import ManageHotels from './Components/Dashboard/AdminDashboard/ManageCountries/ManageHotels';
+import NotFound from './Pages/NotFound/NotFound';
+import Home from './Pages/Home/Home';
 
 function App() {
 
