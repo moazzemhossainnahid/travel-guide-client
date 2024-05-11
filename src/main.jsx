a0dac7df89c25aeb286139e0e6276b0e1a4e7000
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-05-11 23:13:59 +0600

    initiate

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
